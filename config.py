58433a22c9ff406338cf7dd1d662a7d4555ebf36
mr.joshuagordon@gmail <mr.joshuagordon@gmail.com>
2025-02-27 10:34:56 -0600

    Add config.py to .gitignore and stop tracking it

--- config.py
+++
@@ -1,8 +0,0 @@
-APIFY_API_KEY = "apify_api_W4VogoZ8sw9jMvN60dddFtOUaO4a8t3eorU2"
-TASK_ID = "S3l5dfGYvZTbXzWNr"  # dev
-API_URL = "https://api.apify.com/v2/actor-tasks/mr.joshuagordon~instagram-post-scraper-task---dev/runs/last/dataset/items"
-API_URL = "https://api.apify.com/v2/actor-tasks/mr.joshuagordon~scrape-popular-triathlon-instagrams/runs/last/dataset/items"
-
-DATA_PATH = "/data/"
-#DATA_PATH = "data/"
-# main TASK_ID =  #"FOhHAQ8pPrqHrdFTY"
